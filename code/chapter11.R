d5c05762fe1982ab844aa14b90d053aad9f7dfd6
yuyangstatistics <yuyangstatistics@outlook.com>
2022-11-27 23:02:51 -0600

    update chapter 11

--- code/chapter11.R
+++ code/chapter11.R
@@ -14,3 +14,15 @@ pchisq(24, df = 13, lower.tail = FALSE)
 dat <- matrix(c(238, 247, 374, 810), nrow = 2, byrow = TRUE)
 dat
 chisq.test(dat, correct = FALSE)
+
+(mytest <- chisq.test(dat, correct = FALSE))
+mytest$expected
+
+# An Example on the survey data
+survey_f22 <- read.csv("http://users.stat.umn.edu/~parky/Fall2022Survey.csv", 
+                       header = TRUE)
+freq.table <- table(survey_f22$FavSeason, survey_f22$Shower)
+freq.table
+mytest <- chisq.test(freq.table, correct = FALSE)
+mytest
+mytest$expected
